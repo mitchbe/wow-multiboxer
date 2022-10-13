fc7bdc39beda0fa45907f28aafef9415c087b929
mitchbe <mitchell.beaumont@gmail.com>
2022-10-13 21:31:04 +1000

    minor change

--- tshamanbot.py
+++ tshamanbot.py
@@ -9,7 +9,7 @@ from botlib import *
 
 class ClientFrame(BotFrame):
     def __init__(self, *args, **kw):
-        super(PriestFrame, self).__init__(*args, **kw)
+        super(ClientFrame, self).__init__(*args, **kw)
 
     def addButtons(self):
         pnl = wx.Panel(self)
@@ -24,7 +24,7 @@ class ClientFrame(BotFrame):
         b_eat      = wx.Button(pnl, wx.ID_ANY, "Eat")
         b_drink    = wx.Button(pnl, wx.ID_ANY, "Drink")
         b_rot1     = wx.ToggleButton(pnl, wx.ID_ANY, "DPS Assist")
-        b_rot2     = wx.ToggleButton(pnl, wx.ID_ANY, "Null")
+        #b_rot2     = wx.ToggleButton(pnl, wx.ID_ANY, "Null")
 
         b_healme.Bind(wx.EVT_BUTTON, lambda e : Keyboard.press("F2 6")); 
         b_healself.Bind(wx.EVT_BUTTON, lambda e : Keyboard.press("F1 6")); 
@@ -34,15 +34,17 @@ class ClientFrame(BotFrame):
         b_eat.Bind(wx.EVT_BUTTON, lambda e : Keyboard.press("-")); 
         b_drink.Bind(wx.EVT_BUTTON, lambda e : Keyboard.press("=")); 
         b_rot1.Bind(wx.EVT_TOGGLEBUTTON, lambda e : self.keyboard.rotate("shaman/dpsassist") if e.IsChecked() else self.keyboard.stopRotate()); 
-        b_rot2.Bind(wx.EVT_TOGGLEBUTTON, lambda e : self.keyboard.rotate("rotation2") if e.IsChecked() else self.keyboard.stopRotate()); 
+        #b_rot2.Bind(wx.EVT_TOGGLEBUTTON, lambda e : self.keyboard.rotate("rotation2") if e.IsChecked() else self.keyboard.stopRotate()); 
 
         sizer.Add(b_healme)
         sizer.Add(b_healself)
         sizer.Add(b_follow)
         sizer.Add(b_stop)
         sizer.Add(b_jump)
+        sizer.Add(b_eat)
+        sizer.Add(b_drink)
         sizer.Add(b_rot1)
-        sizer.Add(b_rot2)
+        #sizer.Add(b_rot2)
 
         pnl.SetSizerAndFit(sizer)
         sizer.SetSizeHints(self)
