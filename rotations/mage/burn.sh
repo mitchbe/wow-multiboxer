bda88e6501bb6fd1a35c7f21bf370b2202ebcb73
mitchbe <mitchell.beaumont@gmail.com>
2022-10-22 23:53:47 +1000

    minor change

--- rotations/mage/burn.sh
+++ rotations/mage/burn.sh
@@ -3,9 +3,11 @@ source rotation-lib.sh
 
 keyboard_press "s F2 f"
 keyboard_press "1"
-sleep 1.$(rand 55 99)
+sleep 2.$(rand 5 25)
+keyboard_press "3"
+sleep 1
 while true 
 do
 	keyboard_press "2"
-	sleep 1.$(rand 55 99)
+	sleep 1.$(rand 5 25)
 done
